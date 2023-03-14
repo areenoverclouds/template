a3083df35905f76d86902b3d24546c0737159da5
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2023-03-14 23:19:21 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int        long long
+#define pb         push_back
+#define mp         make_pair
+#define ff         first
+#define ss         second
+#define vi         vector<int>
+#define vvi        vector<vector<int>>
+#define all(x)     x.begin(),x.end()
+#define rall(x)    x.rbegin(),x.rend()
+#define debug(x)   cerr<<#x<<" is "<<x<<'\n';
+#define rep(i, n)  for(int i = 0; i < (n); ++i)
+#define uniq(v)    v.erase(unique(all(v)),v.end())
+
+#define yes        cout << "YES\n"
+#define no         cout << "NO\n"
+
+typedef long long ll;
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+const ll MOD = 1e9 + 7;
+const ll INF  = 1e18;
+
+// ascii A-65, a-97, 0-48
+
+/*----------------------------------------------------------------------------*/
+
+void solve(){
+    
+    
+}
+
+/*----------------------------------------------------------------------------*/
+signed main() {
+    
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }    
+	return 0;
+}
